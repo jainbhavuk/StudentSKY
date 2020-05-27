4bff80d2d412f83ccdf89b570a30d09d04c9e21d
jainbhavuk <39909353+jainbhavuk@users.noreply.github.com>
2020-05-28 00:58:35 +0530

    Add files via upload

---
+++ Wall Of Fame/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+
+<head>
+<link rel="stylesheet" href="wofcss.css">
+    <!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>StudentSKY | Wall Of Fame</title>
+	<meta name="author" content="Bhavuk Jain">
+	<meta name="keywords" content="sd,sd College, sd Chandigarh, Student SKY, Student Portal sd, Student SKY Online, Web Portal sd, sd wall of fame, sd fame, sd college students, Gallery StudentSky"
+
+
+	
+	
+</head>
+
+<body>
+			<div class="logo"><br><img src="logo.png" width="143" height="155" >
+				<h2>&nbsp;&nbsp;Student SKY's</h2>
+				<h1>WALL OF FAME</h1>
+				<span id="photo"><img src="photo.png" width="145" height="130"></span>
+			</div>
+			
+			<div class="bodywof"> 
+			<hr size="10" color="white" width="99%">
+		<center>	<div class="tab">
+		<table name="photos" cellspacing="5" cellpadding="10">
+		<tr><th> </th>
+			<th> </th>
+			<th> </th>
+	</tr>
+
+
+	<tr>
+		<td><a href ="images/img1.jpg" download ><img src="images/img1.jpg"> </a><h4><a href="https://www.instagram.com/jainbhavuk555" target="_blank"> @jainbhavuk555 </h4></a></td>
+		<td><a href ="images/img2.jpg" download ><img src="images/img2.jpg"></a><h4><a href="https://www.instagram.com/jainbhavuk555" target="_blank"> @student1	</h4></a></td>
+		<td><a href ="images/img3.jpg" download ><img src="images/img3.jpg"></a><h4><a href="https://www.instagram.com/jainbhavuk555" target="_blank"> @user1	</h4></a></td>
+</tr>
+
+<tr>
+	
+		<td><a href ="images/img4.jpg" download ><img src="images/img4.jpg"></a><h4><a href="https://www.instagram.com/jainbhavuk555" target="_blank">@jainbhavuk555	</h4></a></td>
+		<td><a href ="images/img5.jpg" download ><img src="images/img5.jpg"></a><h4><a href="https://www.instagram.com/jainbhavuk555" target="_blank"> @jainbhavuk555 </h4></a></td>
+		<td><a href ="images/img6.jpg" download ><img src="images/img6.jpg"></a><h4><a href="https://www.instagram.com/jainbhavuk555" target="_blank"> @jb555	</h4></a></td>
+</tr>
+
+</table></div>
+ </center>
+
+<hr size="10" color="white" width="99%">
+
+
+<div class="submit">
+Want To Get Featured Here? 
+<br>
+
+<a href = "mailto:studentskysdc@gmail.com" ><h3>Email Your Photographs Here</h3></a>
+<a href= "https://www.instagram.com/sdc32chandigarh" target="_blank"><h3>DM On Our Official Instagram Handle</h3></a>
+
+</div>
+<br>
+<div id="link">
+<a href="../index.php">Back To Home</a></div>
+</div>
+		
+
+<!--
+	<div class="footerdown">
+		<br>
+		<a href="../index.php">Back To Home</a>
+		<br>
+		<br>
+		<h2>&copy;2020 StudentSKY</h2>
+		</div>
+
+	
+-->
+	
+</body>
+</html>
