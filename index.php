01a11eb9627728ced4ddfa78a3007d7f80f1e347
jainbhavuk <39909353+jainbhavuk@users.noreply.github.com>
2020-05-28 00:25:50 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,391 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>SDC - StudentSKY</title>
+
+<meta charset="utf-8">
+<meta name="keywords" content="sdsky.online,sdc,sdsky,sd sky, sdcollege, sd college, sd college chandigarh, student sky, studentsky, student portal, student sky sdc, sd college student, studentsky sdcollege,  chandigarh colleges, student zone, student interface/>
+<!--Author Details-->
+<meta name="author" content="Bhavuk Jain">
+<!--Links to Styling Sheets-->
+<link rel="stylesheet" href="css/style.css">
+<link rel = "stylesheet" href="css/font-awesome.min.css">
+<!-- /Move-Top JS-->
+
+	 
+
+	 	
+
+
+
+</head>
+
+<body link="white" alink="white" vlink="white">
+
+
+
+<!--CSS Spinner-->
+
+
+<!--
+<div class="spinner-wrapper">
+	<div class="spinner"></div>
+	</div>
+-->
+	<div class="logoback">
+			<center><img src="./images/logo.png" class="logo"height="20px"width="30px"></center>
+</div>
+
+
+<!-- banner -->
+
+<div class="studentsky-banner">
+					
+<div class="container">
+<div class="navigation"><center>
+<a href = "#sylanch" > Syllabus </a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href = "#mstanch" > MST </a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href = "#puexam" > Semester </a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href = "Wall Of Fame/index.php" > Wall Of Fame </a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href = "responsiveform/index.php" > Newsletter </a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href = "notices/notices.html" > Notices </a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</center>
+</div></div></div>
+
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    
+</center>
+
+<div id="imgslider">
+<figure>
+<img src = "images/sd1.jpg">
+<img src = "images/image4.jpg">
+<img src = "images/image3.jpg">
+<img src = "images/image2.jpg">
+<img src = "images/image5.jpg">
+</figure>
+</div>
+
+
+
+
+
+
+
+	<br><br><br><br>
+						
+						
+						
+						
+</div>
+</div>
+<center>
+	<div class="sdlove">
+<h1>SDC IS <i class="fa fa-heart" style="font-size:36px;color:red"></i> </h1></div>
+</center>
+
+
+
+
+		
+
+
+<div class="resultnotif">
+		<font color="white" size="5px"> <a href="result/results.html"><BR><div class="innerbox1">CHECK DECEMBER 2019 RESULTS HERE<BR></a></font></div>
+	<br>
+	</div>
+		
+		
+
+
+
+<!-- About Us -->
+<div class="aboutus" id="syllabus">
+
+<h3>SYLLABUS 2020-2021
+</h3>
+<a name="sylanch"></a>
+<br>
+<DIV class="ss_dwn">
+	<table  bordercolor="white" bgcolor="yellow" CELLPADDING="10" border="10">
+		<tr><a name="navsyl"></a>
+<td align="center"class="stream"><b>S T R E A M</b></th> 
+
+<td align="center"class="linkst"><B>L I N K</B></th>
+</tr>
+
+<tr>
+<td align="center"class="colmix1" >B.A.(Eco. Hons.)</td>
+
+<td align="center"class="colmix"><a href="syllabus/ba eh/syllabusnav.html">Download</a></td>
+</tr>
+<tr>
+	<td align="center"class="colmix1">B.B.A.</td>
+	
+	 <td align="center"class="colmix"><a href="syllabus/bba/syllabusnav.html">Download</a></td>
+	</tr>
+
+	<tr>
+		<td align="center"class="colmix1">B.Sc. / B.A. General</td>
+		 
+		 <td align="center"class="colmix"><a href="syllabus/bsc ba gen/syllabusnav.html">Download</a></td>
+		</tr>
+		<tr>
+		<td align="center"class="colmix1">B.Sc. / B.A. Hons.</td>
+		 
+		 <td align="center"class="colmix"><a href="syllabus/bsc ba hons/syllabusnav.html">Download</a></td>
+		</tr>
+
+	<tr>
+		<td align="center"class="colmix1">B.Sc. Bio-Technology</td>
+		 
+		 <td align="center"class="colmix"><a href="syllabus/bsc biotech/syllabusnav.html">Download</a></td>
+		</tr>
+	<tr>
+		<td align="center"class="colmix1">B.Sc. Bio-Informatics</td>
+		 
+		 <td align="center"class="colmix"><a href="syllabus/bsc bioinfo/syllabusnav.html">Download</a></td>
+		</tr>
+		<tr>
+			<td align="center"class="colmix1">B.C.A.</td>
+			 
+			 <td align="center"class="colmix">
+				 <A HREF="syllabus/bca/syllabusnav.html">Download</a></td>
+			</tr>
+			<tr>
+					<td align="center"class="colmix1">B.Com. General</td>
+					 
+					 <td align="center"class="colmix">
+						 <A HREF="syllabus/bcom/syllabusnav.html">Download</a></td>
+					</tr>
+			
+					<tr>
+					<td align="center"class="colmix1">B.Com. Hons.</td>
+					 
+					 <td align="center"class="colmix">
+						 <A HREF="syllabus/bcom hons/syllabusnav.html">Download</a></td>
+					</tr>
+				<tr>
+					<td align="center"class="colmix1">M.A. Economics</td>
+					 
+					 <td align="center"class="colmix"><a href="syllabus/ma economics/syllabusnav.html">Download</a></td>
+					</tr>
+					<tr>
+					<td align="center"class="colmix1">M.A. English</td>
+					 <td align="center"class="colmix"><a href="syllabus/ma english/syllabusnav.html">Download</a></td>
+					</tr>
+					<tr>
+						<td align="center"class="colmix1">M.Com.</td>
+						 
+						 <td align="center"class="colmix"><a href="syllabus/mcom/syllabusnav.html">Download</a></td>
+						</tr>
+						<tr>
+						<td align="center"class="colmix1">M.Com. EFB</td>
+						 
+						 <td align="center"class="colmix"><a href="syllabus/mcom efb/syllabusnav.html">Download</a></td>
+						</tr>
+						
+							
+								<tr>
+									<td align="center"class="colmix1">M.Sc. IT</td>
+									 
+									 <td align="center"class="colmix"><a href="syllabus/msc it/syllabusnav.html">Download</a></td>
+									</tr>
+									<tr>
+									<td align="center"class="colmix1">M.Sc. Physics</td>
+									 
+									 <td align="center"class="colmix"><a href="syllabus/msc physics/syllabusnav.html">Download</a></td>
+									</tr>
+									<tr>
+									<td align="center"class="colmix1">M.Sc. BioTech</td>
+									 
+									 <td align="center"class="colmix"><a href="syllabus/msc biotech/syllabusnav.html">Download</a></td>
+									</tr>
+									<tr>
+									<td align="center"class="colmix1">M.Sc. BioInfo</td>
+									 
+									 <td align="center"class="colmix"><a href="syllabus/msc bioinfo/syllabusnav.html">Download</a></td>
+									</tr>
+									<tr>
+									<td align="center"class="colmix1">M.Sc. Applied Chem.</td>
+									 
+									 <td align="center"class="colmix"><a href="syllabus/msc appchem/syllabusnav.html">Download</a></td>
+									</tr>
+									<tr>
+									<td align="center"class="colmix1">PGDCA</td>
+									 
+									 <td align="center"class="colmix"><a href="syllabus/pgdca/syllabusnav.html">Download</a></td>
+									</tr>
+									<tr>
+									<td align="center"class="colmix1">PGDMM</td>
+									 
+									 <td align="center"class="colmix"><a href="syllabus/pgdmm/syllabusnav.html">Download</a></td>
+									</tr>
+									
+
+</table>
+</DIV>
+</div>
+<!-- /About Us -->
+<br><br><br><br><br><br><br><br>
+<!-- M-S-T -->
+<div class="mst" id="mst">
+<div class="container">
+<a name="mstanch"></a>
+<h3>Mid-Semester Test</h3>
+<p class="agileits">Preparing for MST? <br>
+
+<div class="col-md-6 agileits">
+<div class="col-md-2 ss_ic">
+	<center>
+			<i class="fa fa-clock-o" style="font-size:48px;color:red" aria-hidden="true"></i>
+</div> 
+
+<div class="col-md-10 ss_pgh">
+			
+	<h4>
+		
+		<a href="mst/shifts.html"><font color="white"><u>Date-Sheet</u></font></h4>
+<p class="ssl_prgh">Check The Date-Sheet Here!	</p></a>
+	</div>
+	<div class="clearfix"></div>
+	</div>
+	<div class="col-md-6 agileits">
+	<div class="col-md-2 ss_ic"><CENTER>
+		<a href="mst/shifts.html">
+			<i class="fa fa-file-text-o" style="font-size:48px;color:red" aria-hidden="true"></i>
+	</div> 
+
+
+	<div class="col-md-10 ss_pgh">
+			
+<h4>
+	
+	<a href="mst/shifttime.html"><font color="white"><u>SHIFTS/SEATING</u></font></h4>
+<p class="ssl_prgh">Don't forget to check where you have to be seated for your MSTs. After all it's your time on stake! </p></a>
+</div>
+<div class="clearfix"></div>
+</div>
+<div class="col-md-6 agileits">
+<div class="col-md-2 ss_ic"><CENTER>
+	<a href="mst/syllabusmst.html">
+		<i class="fa fa-file-text-o" style="font-size:48px;color:red" aria-hidden="true"></i>
+</div> 
+<div class="col-md-10 ss_pgh">
+<h4 style="color:white;"><u>Syllabus</u></h4>
+<p class="ssl_prgh">Be Sure To Cover Up Your Texts And Study The Accurate Syllabus! </p></a>
+</div>
+</div>
+<br><br><br><br><br><br><br>
+
+</div>
+</div>
+<a name="puexam">
+</center>
+<!-- /mst -->
+<!-- PU EXAMINATION-->
+
+<div class="exam" id="exam">
+		<div class="container"><font color="black">
+		<h3 style="color:black;">Semester Exams</h3>
+	<p class="agileits" style="color:black;"> It's Your Final Exams And We Know You Are Up For It Earnestly! Aren't You? Have a RedBull And Chill! We've Got Everthing Else Figured Out.</p>
+		<div class="col-md-6 agileits">
+		
+		<div class="col-md-2 ss_ic"></a>
+			<center>
+					<i class="fa fa-rupee" style="font-size:48px;color:red"></i>
+		</div> 
+		
+		
+			<div class="col-md-10 ss_pgh">
+					
+		<h4 style="color:black;">
+			
+			<a target="_blank" href="payment_form_widget\web\index.html"><u>Examination Fee/MST Fine</u></font></h4>
+		<p class="ssl_prgh" style="color:black;">Pay The Semester Examination Fees Well In Time! </p></a>
+		</div>
+		<div class="clearfix"></div>
+		</div>
+<br>
+		<div class="col-md-10 ss_pgh">
+				<center><i class="fa fa-spinner fa-spin" style="font-size:48px; color:red;"></i></center>	
+			<h4 style="color:black;">
+				
+				<a href="Practicals/practical.html"><u>Practicals Date-Sheet</u></font></h4>
+			<p class="ssl_prgh" style="color:black;">Check The Practical Examination Date-Sheet </p></a>
+			</div>
+			<div class="clearfix"></div>
+			</div>
+
+		<div class="col-md-6 agileits">
+		<div class="col-md-2 ss_ic"><CENTER>
+			<a href="#navsyl">
+				<i class="fa fa-file-text-o" style="font-size:48px;color:red" aria-hidden="true"></i>
+		</div> 
+		<div class="col-md-10 ss_pgh">
+					
+			<h4 style="color:black;">
+				
+				<a  href="TheoryExam/theory.html"><u>Theory Exam Date-Sheet</u></font></h4>
+			<p class="ssl_prgh" style="color:black;">Check The Final Theory Examination Date-Sheet </p></a>
+			</div>
+			<div class="clearfix"></div>
+			</div>
+		</div>
+		</div>
+		</center>
+	
+<!-- /PU EXAMINATION-->
+
+
+
+
+<!-- Footer-->
+<div class="breakspace">
+<br>
+</div>
+<div class="footer">
+	
+<div class="container">
+		<div class="col-md-4 agile_l">
+			<a name="disclaimer"></a>
+<center><h3 style="align:center;">D I S C L A I M E R</h3>
+<p class="ss_pght" >StudentSKY Unified Platform <i>"Shall Not, In Any Case, Be Mis-Understood" </i> As A Replacement/Replica To The STUDENT PORTAL <br><a href="https://www.tcsion.com/SelfServices/">(TCSion)</a> or the Official Website <a href="http://ggdsd.ac.in">(GGDSD.AC.IN)</a> Of S.D. College, Sector-32, Chandigarh. This Platform <i>"Does NOT Collect Any Fees Or Confidential Information"<br> </i>From The Students On Behalf Of The College. This Platform Runs Independently <i>Without Any GRANT/SUPPORT<br></i> From Any Institution/Association/Organisation. For More Information, <br>Kindly Visit Our POLICIES PAGE.<br><a href="POLICIES/policies.html"> <font size="4"><u><b>Policies Page</b></u></a></font></center></p>
+<a href="#"></a>
+</div>
+<center> 
+<h3><i class="fa fa-instagram"> Follow Us On Instagram </i><br>
+<a href="https://instagram.com/sdc32chandigarh/">@sdc32chandigarh</a></center></h2>
+</div>
+
+
+
+</div>
+
+<div class="clearfix"></div>
+</div>
+
+</div>	
+
+
+
+
+	
+
+	
+	
+		<div class = "breakspace2">
+		<br>
+		</div>
+				
+</div>
+<div class="visitorbelow"><br><br>
+<div class="copyclaim">
+<h2>&copy;2020 StudentSKY</h2>
+<h4>Handcrafted With <i class="fa fa-heart"></i></h4>
+</div>
+<br>
+</div>
+</body>
+</html>
